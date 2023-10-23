d1e70ac89395f0b21a750617c47ded5d519091ce
Felipe Maia <felipemaia850@gmail.com>
2023-10-23 02:14:23 +0000

    changed `baseURL`

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // json-server --watch db.json
 export const api = axios.create({
-  baseURL: "https://projeto-carrinho-navy.vercel.app",
+  baseURL: "http://localhost:3000",
 });
